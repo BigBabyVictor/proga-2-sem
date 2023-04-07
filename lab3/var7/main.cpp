771780b3445ba82c3a9d1487a244a4319b572387
libertalec <96635750+BigBabyVictor@users.noreply.github.com>
2023-04-07 20:34:24 +0300

    Update main.cpp

--- lab3/var7/main.cpp
+++ lab3/var7/main.cpp
@@ -2,8 +2,10 @@
 #include "IntegerSet.h"
 
 int main() {
+    // Создание двух множеств
     IntegerSet set1, set2;
 
+    // Добавление чисел в множества
     set1 += 1;
     set1 += 3;
     set1 += 5;
@@ -12,12 +14,15 @@ int main() {
     set2 += 3;
     set2 += 5;
 
+    // Вывод множеств на экран
     std::cout << "Set 1: " << set1 << std::endl;
     std::cout << "Set 2: " << set2 << std::endl;
 
+    // Объединение множеств и вывод результата на экран
     IntegerSet unionSet = set1 + set2;
     std::cout << "Union of Set 1 and Set 2: " << unionSet << std::endl;
 
+    // Сравнение множеств на равенство и вывод результата на экран
     if (set1 == set2) {
         std::cout << "Set 1 is equal to Set 2" << std::endl;
     } else {
